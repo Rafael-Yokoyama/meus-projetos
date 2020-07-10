64131035e3ee14a396220301583f98a710fa187d
Rafael <rafaelyokoyama925@gmail.com>
2020-07-10 14:21:10 -0300

    criado a interface
    criado a interface do app

---
+++ Mensagens Inspiradoras/lib/Home.dart
@@ -0,0 +1,226 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  void _esperaca(){
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+      
+                       
+      appBar: AppBar(
+        centerTitle: true,
+      
+        
+        title:Text(
+          "   MENSAGENS INSPIRADORAS  " ,
+          style: TextStyle(
+            color: Colors.white
+            
+
+                     
+
+          ),
+                         
+                
+        ) , 
+        backgroundColor: Colors.black
+
+      ) ,
+      
+
+      body:Container(
+      
+        
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(15),
+          
+          child: Column(
+
+           crossAxisAlignment: CrossAxisAlignment.center,
+
+          mainAxisAlignment:MainAxisAlignment.center ,
+                          
+               
+                       
+          children: <Widget>[
+            
+                   
+          Padding(padding:EdgeInsets.all(12) ,
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            
+            
+            children: <Widget>[
+
+            GestureDetector(
+              onTap: _esperaca,
+              
+              child:
+      
+                     Image.asset("images/esperanca.png",
+                     cacheWidth: 130,
+
+                   
+                              
+          ), ),
+            GestureDetector( 
+
+              onTap: _esperaca,
+            
+              child:
+                      Image.asset("images/alegria.png",
+                       cacheWidth: 130,
+          
+                              
+          ), ),
+         
+         
+
+          ],),
+          
+                   
+          
+          ),
+
+          Padding(padding:EdgeInsets.all(12 ),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            
+            
+            children: <Widget>[
+
+            GestureDetector(  
+                 child:
+                          Image.asset("images/confianca.png",
+                          cacheWidth: 130,
+                         
+          
+          ), ),
+          GestureDetector( 
+                 child:
+
+                            Image.asset("images/virtude.png",
+                            cacheWidth: 130,
+        
+                              
+          ), ),
+         
+         
+          ],),
+          
+                   
+          
+          ),
+           Padding(padding:EdgeInsets.all(12 ),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            
+            
+            children: <Widget>[
+
+            GestureDetector(  
+                         child:
+
+                                   Image.asset("images/fe.png",
+                                   cacheWidth: 130,
+                         
+          
+          ), ),
+          GestureDetector( 
+            child:
+
+                                    Image.asset("images/amor.png",
+                                    cacheWidth: 130,
+        
+                   
+          ), ),
+         
+         
+
+          ],),
+          
+          
+                   
+          ),
+            Padding(padding:EdgeInsets.all(12 ),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            
+            
+            children: <Widget>[
+
+            GestureDetector( 
+              child:
+
+                                 Image.asset("images/paciencia.png",
+                                 cacheWidth: 130,
+        
+                    
+          
+          ), ),
+          GestureDetector( 
+              child:
+
+                                 Image.asset("images/caridade.png",
+                                  cacheWidth: 130,
+        
+          
+                    
+          ), ),
+         
+         
+
+
+          ],),
+          
+          
+          
+          
+          ),
+          
+
+                    
+        ],
+        ),
+
+
+
+
+        ),
+
+        
+       
+                                           
+                     
+
+                       
+                  
+                      
+
+
+
+      ), 
+           
+
+            
+    );
+  }
+}
---
+++ Mensagens Inspiradoras/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:escrituras/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Home() ,
+    debugShowCheckedModeBanner: false,
+
+
+  ));
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+    
+    
+
